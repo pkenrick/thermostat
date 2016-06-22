0bd82c9fb8850e26dcfccf28e7d56376f4802c24
paulkenrick <pkenrick@gmail.com>
2016-06-22 14:50:20 +0100

    Added minimum temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,8 +1,12 @@
 'use strict';
 
 describe('Thermostat', function(){
+  
   var thermostat;
-  thermostat = new Thermostat();
+  
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
 
   it('thermostat is created with a temperature of 20 degrees', function(){
     expect(thermostat.temperature).toEqual(20);
@@ -12,4 +16,17 @@ describe('Thermostat', function(){
     thermostat.increase();
     expect(thermostat.temperature).toEqual(21);
   });
+
+  it('decreases temperature', function(){
+    thermostat.decrease();
+    expect(thermostat.temperature).toEqual(19);
+  });
+
+  it('has a minimum temperature of 10', function(){
+    for(var i = 0; i < 10; i++) {
+      thermostat.decrease();
+    };
+    expect(function(){thermostat.decrease()}).toThrowError('Can\'t decrease below 10 degrees');
+  });
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,8 +2,22 @@
 
 function Thermostat(){
   this.temperature = 20;
+  this.MINIMUM_TEMP = 10;
 };
 
-Thermostat.prototype.increase = function(){
-  this.temperature += 1;
+Thermostat.prototype = {
+  increase: function() {
+    this.temperature += 1;
+  },
+  
+  decrease: function() {
+    if (this.atLimit()) {
+      throw new Error('Can\'t decrease below 10 degrees')
+    }
+    this.temperature -= 1;
+  },
+
+  atLimit: function() {
+    return ((this.temperature) <= this.MINIMUM_TEMP);
+  }
 };
