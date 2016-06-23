4c4507841a80f41a81f1e0871d2ce54122559fcd
LukaszGr <lukasz.grychowski@gmail.com>
2016-06-23 17:11:40 +0100

    api added

--- manager.js
+++ manager.js
@@ -28,9 +28,25 @@ $(document).ready(function() {
    thermostat.switchPowerSavingModeOff();
  });
 
+
+
+ $("#current-city").change(function () {
+   var city = $("#current-city").val();
+   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city  + '&appid=1ba251876150a3d6b1093f2689309918&units=metric', function(data) {
+   $("#outside-weather").text(data.main.temp);
+   });
+ });
+
+
+
+
+
+
+
+
  function updateTemp(){
   $("#temperature").attr('class', thermostat.energyUsage());
-  $("#temperature").text(thermostat.temperature); 
+  $("#temperature").text(thermostat.temperature);
  };
 
 });
