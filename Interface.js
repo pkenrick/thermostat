58eddd9d93f8bef22f0b4c8b874f3fe6f3491bc6
paulkenrick <pkenrick@gmail.com>
2016-06-24 11:22:06 +0100

    small changes

--- Interface.js
+++ Interface.js
@@ -33,6 +33,20 @@ $(document).ready(function() {
     };
   };
 
+  $('#selected-city').change(function(){
+    var city = $('#selected-city').val();
+    displayWeather(city);
+  });
+
+  function displayWeather(city){
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data){
+      $('#outside-temp').text(data.main.temp);
+    });
+  };
+
   function updateTemp(){
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.displayUsage());
